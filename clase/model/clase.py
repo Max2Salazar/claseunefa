47fdb1981852c38552d6e06cdaf9995db84ea4c3
Jeison Pernia <jeisonestudio@gmail.com>
2017-11-03 11:59:13 -0400

    campos many2one y one2mamy

--- clase/model/clase.py
+++ clase/model/clase.py
@@ -13,4 +13,26 @@ class claseunefa(models.Model):
     
 
     name = fields.Char(string='Estudiante', required=True, readonly=False,)
+    partner_id = fields.Many2one('res.partner', string='Cliente',)
+    semestre_id = fields.Many2one('clase.semestre', string='Semestre',)
     
+    materias = fields.One2many('clase.materias', 'unefa_id', string='Order Lines', )
+
+    
+    
+    
+class claseunefa1(models.Model):
+    _name = "clase.semestre"
+    
+
+    name = fields.Char(string='Semestre', required=True, readonly=False,)
+    
+class claseunefa2(models.Model):
+    _name = "clase.materias"
+    
+
+    name = fields.Char(string='Materias', required=True, readonly=False,)
+    uc = fields.Char(string='Unidades', required=True, readonly=False,)
+    
+    unefa_id = fields.Many2one('clase.unefa"', string='Unefa',)
+   
